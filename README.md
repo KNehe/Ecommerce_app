87fe45a680b6473620fa0fde84dfe28f75a052b6
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2020-09-21 16:33:05 +0300

    first commit: folder structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ecommerce App
+
+An ecommerce app built using Flutter
+
+To run open terminal and execute 'flutter run' command
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(EcommerceApp());
+}
+
+class EcommerceApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ecommerce app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Material()
+    );
+  }
+}
+
