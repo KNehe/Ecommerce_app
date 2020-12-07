b909f31f3b35c2da06b063433d2250b4a95f2b20
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2020-12-07 13:54:36 +0300

    drawer width

--- lib/widgets/guest_user_drawer_widget.dart
+++ lib/widgets/guest_user_drawer_widget.dart
@@ -14,7 +14,7 @@ class GuestUserDrawerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height / 5),
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.3),
       child: Column(
         children: [
           Text(
